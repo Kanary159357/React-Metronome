c37b53f3aee4fd7140c1e129aaa87f4d9626a436
kanary <kanary159357@gmail.com>
2021-03-06 12:12:37 +0900

    colorify

--- src/components/BeatView.js
+++ src/components/BeatView.js
@@ -4,7 +4,7 @@ import BeatBall from './BeatBall';
 const BeatViewBlock = styled.div`
     width:100%;
     height: 60vh;
-    background: #f7f7f7;
+    background: #FEF3C8;
     display:flex;
     justify-content:space-around;
     align-items:center;
@@ -12,7 +12,7 @@ const BeatViewBlock = styled.div`
 const Box = styled.div`
 width: 75px;
 height: 75px;
-background: ${props=>props.on ? "#ffa069": "tomato"};
+background: ${props=>props.on ? "#FBAB6F": "#EC8953"};
 border-radius:50%;
 `
 const TapButton = styled.div`
@@ -21,12 +21,13 @@ const TapButton = styled.div`
   width: 10%;
   height:10%;
   position: absolute;
-  background: coral;
+  background: #EC8953;
   font-size: 1.5em;
   display:flex;
   justify-content:center;
   align-items:center;
   color: #fff;
+  user-select:none;
   border-radius: 0 0 15px;
 `
 
--- src/components/ControlView.js
+++ src/components/ControlView.js
@@ -4,49 +4,54 @@ const ControlViewBlock = styled.div`
     height: 40vh;
     margin-top:-32px;
     width: 100%;
-    background: yellowgreen;
+    background: #BDDDBF;
     display:flex;
     flex-direction:column;
     justify-content:center;
     align-items:center;
+    user-select: none;
 `
 const ControlSlider = styled.input`
    -webkit-appearance: none;  
   appearance: none;
   width: 50%; 
   height: 25px; 
-  background: #d3d3d3; 
+  background: #7EA285; 
   outline: none; 
   opacity: 0.7; 
   -webkit-transition: .2s; 
   transition: opacity .2s;
   &:hover{
-    background: #d4d4d4;
+    background: #97ba9e;
   }
   ::-webkit-slider-thumb{
     -webkit-appearance: none;
   appearance: none;
   width: 25px;
   height: 25px;
-  background: #4CAF50;
+  background: #D9EDDE;
   cursor: pointer;
   }
   ::-moz-range-thumb {
   width: 25px;
   height: 25px;
-  background: #4CAF50;
+  background: #D9EDDE;
   cursor: pointer;
 }
 `
 
+
 const StartButton = styled.button`
   width: 50%;
   height: 50px;
-  background: green;
+  background: #465D52;
   border:0;
   &:focus, &:active{
     padding:0;
     outline: none;
+  }
+  &:hover{
+    background:#354a40;
   }
   color: white;
   font-size: 36px;
@@ -68,7 +73,7 @@ const ControlView = ({beats,bpm, setBPM, setBeats, isPlaying, handlePlayToggle})
           {beats} Beats
           <ControlSlider
           type= "range"
-          min = "1"
+          min = "2"
           max= "8"
           value={beats}
           onChange={e=>{
